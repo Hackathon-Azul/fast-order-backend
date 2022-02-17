2ee585ddb5bb49b38688cbad5573df7db43e2470
João Otávio <joaotavioos@outlook.com>
2022-02-17 20:34:27 -0300

    Add User Model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
-class ApplicationController < ActionController::API
+module Authenticatable
+   extend ActiveSupport::Concern
+      
+        included do
+        include DeviseTokenAuth::Concerns::SetUserByToken
+        before_action :authenticate_user!
+   end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+ 
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+  validates :profile, presence: true
+  enum profile: { Admin: 0, Attendant: 1, Cooker: 2 }
+end
