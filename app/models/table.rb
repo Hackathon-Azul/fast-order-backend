3ecb205947b89787914fbcc2fe3aff805231bb3d
João Otávio <joaotavioos@outlook.com>
2022-02-18 21:30:56 -0300

    Generate Table Model

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,5 +1,5 @@
 class Table < ApplicationRecord
-    validates :table_number, presence: true
+    validates :table_number, presence: true, numericality: { only_integer: true, greater_than: 0 }
     validates :avaliable_table, presence: true
 
 end
