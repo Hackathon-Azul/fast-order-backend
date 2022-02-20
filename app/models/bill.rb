5ef4d4efce538aa46c89b5ce71bbdae6358b4ae4
João Otávio <joaotavioos@outlook.com>
2022-02-20 16:21:03 -0300

    Fix set values implementation and add storefront order controller

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,10 +2,11 @@ class Bill < ApplicationRecord
   belongs_to :order
   validates :status_bill, :total, presence: true 
 
-  before_validation :set_price
+  before_validation :set_values
 
-  def set_price
+  def set_values
     total = 0
-    total = order.quantity * order.product.price
+    self.total = order.quantity * order.product.price
+    self.status_bill = true
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,15 +4,18 @@ class Order < ApplicationRecord
   has_many :order_items
 
   enum status: { Waiting: 0, InProgress: 1, Avaliable: 2, Finished: 3, Canceled: 4 }
-  validates :client_name, :status, :order, presence: true
+  validates :client_name, :status, :total, presence: true
   accepts_nested_attributes_for :order_items, allow_destroy: true
+  validates :status, presence: true, on: :update
 
   before_validation :set_price
 
   def set_price
-    total = 0
+    total_value = 0
     order_items.each do |oi|
       total += oi.quantity * oi.product.price
   end
+    self.total = total_value
+end
 
 end
