c2c24cb11bf41570a3603796ada906fbdc30bd44
João Otávio <joaotavioos@outlook.com>
2022-02-21 20:39:56 -0300

    Add users seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,14 @@ Order.destroy_all
 Category.destroy_all
 Bill.destroy_all
 Table.destroy_all
+User.destroy_all
+
+
+puts 'Criando Usuários'
+
+User.create(name: "Larissa", email: "test@test.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Michael", email: "test2@test.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Leandro", email: "test3@test.com", password: "123456", password_confirmation: "123456")
 
 puts 'Criando Categorias'
 
