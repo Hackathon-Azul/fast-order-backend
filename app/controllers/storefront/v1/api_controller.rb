fd7d84cddbf6e78c7feef185e3abf88591b49739
João Otávio <joaotavioos@outlook.com>
2022-02-28 12:46:09 -0300

    Add database cleaner and storefront Authenticatable

--- app/controllers/storefront/v1/api_controller.rb
+++ app/controllers/storefront/v1/api_controller.rb
@@ -2,7 +2,8 @@
 
 module Storefront::V1
   class ApiController < ApplicationController
-    
+  include Authenticatable
+ 
 
     include SimpleErrorRenderable
     self.simple_error_partial = "shared/simple_error"
